9ff6f524cd5fe48730451ccc7230aa3456c75ef9
John Stewart <cane.cubo@gmail.com>
2025-12-31 20:27:34 -0500

    show historgrams; code cleanup

--- src/R/create_network.R
+++ src/R/create_network.R
@@ -1,31 +1,36 @@
 library(tidyverse)
 
-load_gene_info = function() {
-  read_csv('data/gene-info.csv')
-}
+load_gene_info = function() read_csv('data/gene-info.csv')
 
-load_correlations = function () {
-  read_csv('data/corrs.csv') 
-}
+load_correlations = function () read_csv('data/corrs.csv')
+load_corr_matrix = function () load_correlations() |> as.matrix()
 
-connectivity = function(m) {
-  rowSums(m, na.rm=T)
-}
+# --- Network construction ---
+connectivity = function(m) rowSums(m, na.rm=TRUE)
 
-adjacency = function(correlations, beta) {
-  a = abs(correlations)
+adjacency = function(corr_matrix, beta) {
+  a = abs(corr_matrix)
   0 -> a[is.na(a)]
   0 -> diag(a)
 
   a^beta
 }
 
-r_squared = function(m) {
+tom = function(corr_matrix, beta) {
+  a = adjacency(corr_matrix, beta)
+
+  k = connectivity(a)
+  k_min = outer(k, k, pmin)
+
+  (a %*% a + a) / (k_min + 1 - a)
+}
+
+# --- Scale-free topology ---
+scale_free_r2 = function(m) {
   k = connectivity(m)
   binned_k = hist(k, plot=FALSE)
   p_k = binned_k$counts / sum(binned_k$counts)
 
-  # Filter out empty bins
   nonzero = p_k > 0
   p_k[nonzero] -> p_k
   use_k = binned_k$mids[nonzero]
@@ -33,27 +38,15 @@ r_squared = function(m) {
   cor(log10(p_k), log10(use_k))^2
 }
 
-tabulate_betas = function(correlations) {
-  1:20 |>
-    map_dfr(function(beta) {
-      a = adjacency(correlations, beta)
-      r2 = r_squared(a)
-      k = connectivity(a)
-      tibble(beta = beta, r2 = r2, mean_k = mean(k))
-    })
+tabulate_betas = function(corr_matrix, betas = 1:20) {
+  map_dfr(betas, function(beta) {
+    a = adjacency(corr_matrix, beta)
+    tibble(beta = beta, r2 = scale_free_r2(a), mean_k = mean(connectivity(a)))
+  })
 }
 
-minima = function(m) {
-  k = connectivity(m)
-  outer(k, k, pmin)
-}
-
-tom = function(correlations, beta) {
-  a = adjacency(correlations, beta)
-  (a %*% a + a) / (minima(a) + 1 - a)
-}
-
-cluster_tom = function(tom) {
+# --- Clustering ---
+cluster_genes = function(tom) {
   distances = as.dist(1 - tom)
   hclust(distances, method="average")
 }
--- src/R/descriptives.R
+++ src/R/descriptives.R
@@ -1,3 +1,7 @@
+library(ggplot2)
+
+source('src/R/create_network.R')
+
 create_dataset = function(gene_info, correlations) {
   select(gene_info, transcript, is_canonical) |>
     mutate(is_canonical = if_else(is_canonical == "canonical", 1, 0)) -> 
@@ -10,5 +14,16 @@ create_dataset = function(gene_info, correlations) {
     left_join(canonical_lookup, by = c('gene1' = 'transcript')) |>
     rename(is_canonical1 = is_canonical) |>
     left_join(canonical_lookup, by = c('gene2' = 'transcript')) |>
-    rename(is_canonical2 = is_canonical)
+    rename(is_canonical2 = is_canonical) |>
+    mutate(corr_type = case_when(
+      is_canonical1 == 0 & is_canonical2 == 0 ~ "both_no",
+      is_canonical1 == 1 & is_canonical2 == 1 ~ "both_yes",
+      is_canonical1 != is_canonical2 ~ "unsame"))
+}
+
+plot_histograms <- function(x) {
+  ggplot(x, aes(x = correlation, fill=factor(corr_type))) +
+    geom_histogram(show.legend = FALSE) +
+    geom_vline(xintercept = 0.5) +
+    facet_wrap(~corr_type, scales='free_y')
 }
